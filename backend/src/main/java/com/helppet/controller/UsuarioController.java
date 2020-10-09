3d5828f2cb3f993fd444fa5852724adc70416ea0
unknown <vinicius22d@gmail.com>
2020-10-09 12:32:03 -0300

    update Usuario Controller

--- backend/src/main/java/com/helppet/controller/UsuarioController.java
+++ backend/src/main/java/com/helppet/controller/UsuarioController.java
@@ -1,5 +1,54 @@
 package com.helppet.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.helppet.model.UsuarioModel;
+import com.helppet.repository.UsuarioRepository;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
-	//findAll - findById - post - put - delete 
+
+	@Autowired
+	private UsuarioRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<UsuarioModel>> findAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioModel> getByIdUsuario(@PathVariable Long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@PostMapping
+	public ResponseEntity<UsuarioModel> criar(@RequestBody UsuarioModel usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<UsuarioModel> atualizar(@PathVariable Long id, @RequestBody UsuarioModel usuario) {
+		usuario.setIdUsuario(id);
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
+	}
+
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable Long id) {
+		repository.deleteById(id);
+	}
 }
