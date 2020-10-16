b71f7493cca515c11e8a2539c620366f290afa36
Thaina2405 <thaina.silva199757@gmail.com>
2020-10-16 11:36:57 -0300

    insert model UserLogin

--- backend/src/main/java/com/helppet/repository/UsuarioRepository.java
+++ backend/src/main/java/com/helppet/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.helppet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.helppet.model.UsuarioModel;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-
+	public Optional<UsuarioModel> findByEmailUsuario(String emailUsuario);
 }
